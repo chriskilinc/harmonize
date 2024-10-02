8b9433ca6ac36c34d30d379c7aa23295787764e4
Christopher Kilinc <chriskilinc@gmail.com>
2024-10-03 00:00:57 +0200

    Initialize Harmonize using vite

---
+++ README.md
@@ -0,0 +1,4 @@
+# harmonize
+Scale visualization with key and modes selection.
+
+Remake of my old project [scale-theory](https://github.com/chriskilinc/scale-theory).
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <section>
+      <h1>Harmonize</h1>
+    </section>
+  )
+}
+
+export default App
